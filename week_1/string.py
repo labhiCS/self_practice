ca1c3c69a6dce22f7042081197f9571728587733
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-13 23:57:39 +0545

    "First String"

---
+++ week_1/string.py
@@ -0,0 +1,16 @@
+#changing case in string.
+name_0 = "abhishek lamichhane"
+name_1 = "uprndra thapa"
+
+print(name_0.title())
+
+print(name_1.upper())
+
+#combining string
+first_name = "Ram "
+middle_name = "Bahadur"
+Last_name = "Lamichhane"
+
+full_name = first_name + " " + middle_name + " " + Last_name
+print(full_name)
+
