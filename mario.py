0869bcb1ef7a387c6ffc0a5de98eae43f9cb0e34
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-15 22:00:27 +0545

    practice

---
+++ mario.py
@@ -0,0 +1,16 @@
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                break
+        except TypeError:
+            print("That's not integer")
+
+            return n
+main()
