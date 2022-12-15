03e2c6b6f6cd52a62d90e69551c27f7c7a82c5fb
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-15 22:50:44 +0545

    "Dictionaries"

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+favorite_languages = {
+    'Abhi': 'python',
+    'Abhijeet': 'c',
+    'Abhishek': 'ruby',
+    'Anu': 'python',
+ }
+print("Abhishek's favorite language is " + favorite_languages['Abhishek'].title() + ".")
