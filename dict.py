c128e429fb84dc2f7f42646f69c3fe6857f188d8
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-15 22:55:28 +0545

    "dictionary practice"

---
+++ dict.py
@@ -0,0 +1,8 @@
+person_name = {
+    'first_name': 'Abhishek', 
+    'last_name': 'Lamichhane',
+    'age': '19',
+    'city': 'kathmandu',
+    }
+print(person_name["first_name"] + " " + person_name["last_name"]
++ " is " + person_name["age"] + "-yrs old " + ", And he lives in " + person_name["city"] + " city.")
