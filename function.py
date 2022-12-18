f8a00de831a33fa7763cf187dc9a2633c2d80e5a
labhiCS <lamichhaneabhi10@gmail.com>
2022-12-18 23:35:50 +0545

    passing arguments

---
+++ function.py
@@ -0,0 +1,11 @@
+def shirt(size, text_message):
+    """display information about t-shirt"""
+    print("The size of the t-shirt is " + str(size))
+    print("The message printed on a t-shirt is " + text_message.upper())
+
+def main():
+    shirt(20, 'ram')
+    shirt(size = 20, text_message = 'ram')
+
+if __name__ == "__main__":
+    main()
